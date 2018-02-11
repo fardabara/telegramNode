3ab23940def5abc6570dc17cdf2bdbd76789eade
fardabara <farhad_mohammadali@yahoo.com>
2018-02-11 21:31:23 +0330

    Middleware

--- app.js
+++ app.js
@@ -4,14 +4,12 @@ env.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => {
-  console.log('started:', ctx.from.id);
-  return ctx.reply('Welcome!');
+
+bot.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log('Response time %sms', ms);
 });
 
-bot.command('help', (ctx) => ctx.reply('Try send a sticker!'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there!'));
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-
-bot.startPolling();
+// bot.startPolling();
