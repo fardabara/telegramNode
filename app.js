77e2fe69305ee347ff2489b1fcaf1ce89adbd645
fardabara <farhad_mohammadali@yahoo.com>
2018-02-11 22:11:17 +0330

    961122 001

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const Telegraf = require('telegraf');
-const env = require('dotenv');
-env.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+bot.on('message', (ctx) => {
+  // resend existing file by file_id
+  ctx.replyWithSticker('123123jkbhj6b');
 
-bot.use(async (ctx, next) => {
-  const start = new Date();
-  await next();
-  const ms = new Date() - start;
-  console.log('Response time %sms', ms);
+  // send file
+  ctx.replyWithVideo({source: '/path/to/video.mp4'});
+
+  // send stream
+  ctx.replyWithVideo({
+    source: fs.createReadStream('/path/to/video.mp4')
+  });
+
+  // send buffer
+  ctx.replyWithVoice({
+    source: Buffer.alloc()
+  });
+
+  // send url via Telegram server
+  ctx.replyWithPhoto('https://picsum.photos/200/300/');
+
+  // pipe url content
+  ctx.replyWithPhoto({
+    url: 'https://picsum.photos/200/300/?random',
+    filename: 'kitten.jpg'
+  });
 });
 
-// bot.startPolling();
+bot.startPolling();
