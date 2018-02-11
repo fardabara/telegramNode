ccd5420feefbb7c4c95fbe24df958b39301d4fe6
fardabara <farhad_mohammadali@yahoo.com>
2018-02-11 22:24:53 +0330

    961122 express 001

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-const Telegraf = require('telegraf');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+app.set('port', port);
 
-bot.on('message', (ctx) => {
-  // resend existing file by file_id
-  ctx.replyWithSticker('123123jkbhj6b');
-
-  // send file
-  ctx.replyWithVideo({source: '/path/to/video.mp4'});
-
-  // send stream
-  ctx.replyWithVideo({
-    source: fs.createReadStream('/path/to/video.mp4')
-  });
-
-  // send buffer
-  ctx.replyWithVoice({
-    source: Buffer.alloc()
-  });
-
-  // send url via Telegram server
-  ctx.replyWithPhoto('https://picsum.photos/200/300/');
-
-  // pipe url content
-  ctx.replyWithPhoto({
-    url: 'https://picsum.photos/200/300/?random',
-    filename: 'kitten.jpg'
-  });
+app.get('/', (req, res) => {
+  console.log("1111");
+  res.send('222222');
 });
 
-bot.startPolling();
+app.listen(port, () => {
+  console.log(`listen to port ${port}`);
+});
